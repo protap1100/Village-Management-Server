a9455562194fe71e7637ea54f919f4496e6b184e
protap1100 <protapb23@gmail.com>
2024-07-12 22:04:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
+// Custom Middleware
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+
+
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", async (req, res) => {
+  res.send("Meal Buddy Is Running");
+});
+
+app.listen(port, async (req, res) => {
+  console.log(`Meal Buddy Is Running${port}`);
+});
